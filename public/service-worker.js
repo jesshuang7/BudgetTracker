858815efffd0a2cb1c9a6cc046413d6454bb7d8c
Jessica Huang <tzunuhuang@gmail.com>
2021-01-24 12:15:18 +0800

    icons size

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,10 +1,10 @@
 const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
 
-const iconSizes = ["192", "512"];
-const iconFiles = iconSizes.map(
-  (size) => `/icons/icon-${size}x${size}.png`
-);
+// const iconSizes = ["192", "512"];
+// const iconFiles = iconSizes.map(
+//   (size) => `/icons/icon-${size}x${size}.png`
+// );
 
 const staticFilesToPreCache = [
     '/',
@@ -13,7 +13,9 @@ const staticFilesToPreCache = [
     '/db.js',
     '/manifest.webmanifest',
     '/style.css',
-    '/assets/BudgetTracker.gif'
+    '/assets/BudgetTracker.gif',
+    '/icons/icon-192x169.png',
+    '/icons/icon-512x452.png'
 ].concat(iconFiles);
 
 
